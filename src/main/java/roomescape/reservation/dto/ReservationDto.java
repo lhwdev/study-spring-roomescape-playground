622d2e6ed140524408f190d506cb004e551e7349
lhwdev <lhwdev6@outlook.com>
2026-05-04 19:25:57 +0900

    fix: 시간 양식 수정 (생각해보니 포맷이 틀렸더라고요..)

--- src/main/java/roomescape/reservation/dto/ReservationDto.java
+++ src/main/java/roomescape/reservation/dto/ReservationDto.java
@@ -7,6 +7,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class ReservationDto {
+	private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("hh:mm");
+	
 	private final ReservationId id;
 	private final String name;
 	private final LocalDateTime time;
@@ -26,10 +29,10 @@ public class ReservationDto {
 	}
 	
 	public String getDate() {
-		return time.format(DateTimeFormatter.ISO_LOCAL_DATE);
+		return time.format(DATE_FORMAT);
 	}
 	
 	public String getTime() {
-		return time.format(DateTimeFormatter.ISO_LOCAL_TIME);
+		return time.format(TIME_FORMAT);
 	}
 }
