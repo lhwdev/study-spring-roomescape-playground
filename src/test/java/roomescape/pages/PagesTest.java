73d9b6cf1c03d05de3febcc7ae1e8634a8a86db0
lhwdev <lhwdev6@outlook.com>
2026-05-06 09:40:26 +0900

    test: 테스트 구조 개선
    - 테스트를 기능별로 분리
    - 통합 시나리오 테스트의 경우 각 단계별로 메서드 분리

---
+++ src/test/java/roomescape/pages/PagesTest.java
@@ -0,0 +1,21 @@
+package roomescape.pages;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class PagesTest {
+	@Test
+	void 프론트_페이지에_접속_가능() {
+		RestAssured.given()
+				.when().get("/")
+				.then().statusCode(200);
+		
+		RestAssured.given()
+				.when().get("/reservation")
+				.then().statusCode(200);
+	}
+}
