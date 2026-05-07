aa21a087f19c6170d0dea0de7bccf13f69cf6ef9
lhwdev <lhwdev6@outlook.com>
2026-05-07 13:42:10 +0900

    feat: 예약 번호 검증 로직 추가

--- src/main/java/roomescape/reservation/domain/ReservationId.java
+++ src/main/java/roomescape/reservation/domain/ReservationId.java
@@ -5,7 +5,11 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public record ReservationId(@JsonValue long id) implements Comparable<ReservationId> {
 	@JsonCreator
-	public ReservationId {}
+	public ReservationId {
+		if(id <= 0) {
+			throw new IllegalArgumentException("예약 번호는 음수나 0일 수 없습니다.");
+		}
+	}
 	
 	@Override
 	public int compareTo(ReservationId o) {
