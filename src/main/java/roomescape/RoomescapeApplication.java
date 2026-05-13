38d2c44cd8bef4923af2221a9fdf624cb13bd711
lhwdev <lhwdev6@outlook.com>
2026-05-13 12:36:54 +0900

    refactor: H2 데이터베이스를 :8082 포트에서 개방하여 Intellij에서 디버깅

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -1,12 +1,33 @@
 package roomescape;
 
+import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import roomescape.global.DatabaseInitialization;
 
+import java.sql.SQLException;
+
+@SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
 @SpringBootApplication
-public class RoomescapeApplication {
+public class RoomescapeApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(RoomescapeApplication.class, args);
     }
 
+    
+    @Autowired
+    private DatabaseInitialization databaseInitialization;
+    
+    @Override
+    public void run(String... args) {
+        databaseInitialization.initializeTables();
+    }
+    
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2TcpServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "8082");
+    }
 }
